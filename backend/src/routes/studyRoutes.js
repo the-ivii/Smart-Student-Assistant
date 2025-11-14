7b65ef860827c6141e4c705bf3ab7c067fc1870a
Divyanshi <divyanshitalreja12@gmail.com>
2025-11-14 14:00:49 +0530

    Delete backend directory

--- backend/src/routes/studyRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getStudyMaterial } from '../controllers/studyController.js';
-import { optionalAuth } from '../middleware/optionalAuth.js';
-
-const router = express.Router();
-
-router.get('/', optionalAuth, getStudyMaterial);
-
-export default router;
